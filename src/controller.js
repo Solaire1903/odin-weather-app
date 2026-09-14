655d4c4768a8de462a29e4238393551bbf6deb33
Solaire1903 <marvkie@gmail.com>
2026-09-14 14:23:22 +0200

    Add comments

--- src/controller.js
+++ src/controller.js
@@ -1,5 +1,8 @@
 import fetchWeatherData from "./model.js";
 
+/**
+ * Initializes and loads the app
+ */
 const loadApp = async () => {
   console.log(await fetchWeatherData("Essen"));
 };
--- src/model.js
+++ src/model.js
@@ -1,9 +1,14 @@
+/**
+ * Fetches weather data of a given location from the Visual Crossing API
+ * @param {string} location The location to fetch the weather data of
+ * @returns The fetched weather data
+ */
 const fetchWeatherData = async (location) => {
   let weatherData;
 
   try {
     const response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&include=days&key=UWVR4ND6M2ZRGH52PL6SMFYEN&contentType=json`,
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&include=days,current&key=UWVR4ND6M2ZRGH52PL6SMFYEN&contentType=json`,
     );
     if (!response.ok) {
       throw new Error(`Status Code: ${response.status}`);
